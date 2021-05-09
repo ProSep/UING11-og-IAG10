e7351429090d29904500742c0cc87bc5c00ab23a
Subwoofer <driscoll5522@gmail.com>
2021-05-09 15:06:39 +0200

    Updates!

--- uin-base-setup-master/project/client/src/components/content/cardComps/ProductCards.js
+++ uin-base-setup-master/project/client/src/components/content/cardComps/ProductCards.js
@@ -1,13 +1,32 @@
 import Card from './Card';
-import './productcards.css';
-import proImg1 from '../ImagesForPages/pikachu-fig-skeleton.jpg';
-
-const ProductCards = () => (
+import './productcards2.css';
+import Error404 from '../ImagesForPages/404.jpg';
+// import proImg1 from '../ImagesForPages/pikachu-fig-skeleton.jpg';
+const ProductCards = ({ title, body }) => (
+  <div className="card-container">
+    <div className="image-container">
+      <img src={Error404} alt="Testing banner" />
+    </div>
+    <div className="card-content">
+      <div className="card-title">
+        <h3>{title}</h3>
+      </div>
+      <div className="card-body">
+        <p>{body}</p>
+      </div>
+    </div>
+    <div className="btn">
+      <button>
+        <a>View more</a>
+      </button>
+    </div>
+  </div>
+);
+/* const ProductCards = () => (
   <div className="product-card-container">
     <div className="row">
       <Card
         title="This production card title is connected to CARD.js"
-        image={proImg1}
         price="50kr"
         alt="pikachu half skeleton"
       />
@@ -32,5 +51,6 @@ const ProductCards = () => (
     </div>
   </div>
 );
+*/
 
 export default ProductCards;
