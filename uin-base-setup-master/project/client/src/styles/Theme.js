cabe0877cf7f9843cceef188dd36eae5ddfbf573
EricMichaelLP <Eric-m.l.p@hotmail.com>
2021-05-12 19:48:40 +0200

    Scaling jobber med

--- uin-base-setup-master/project/client/src/styles/Theme.js
+++ uin-base-setup-master/project/client/src/styles/Theme.js
@@ -8,13 +8,14 @@ const theme = {
     light: 'rgba(254,254,255,255)',
     brownish: 'rgba(249,249,249,255)',
     red: 'rgb(247, 66, 88)',
-    grey: '#595959'
+    grey: '#595959',
   },
   font: {
     title: 'sans-serif',
   },
   scale: {
-    medium: "min-width: 1500px",
+    medium: 'min-width: 1500px',
+    small: 'max-width: 1110px',
   },
 };
 
