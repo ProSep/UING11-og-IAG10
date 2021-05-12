326746cfd073b07a3f5e02640a655e55065042b6
EricMichaelLP <Eric-m.l.p@hotmail.com>
2021-05-12 20:33:38 +0200

    FooterScales

--- uin-base-setup-master/project/client/src/components/Footer.js
+++ uin-base-setup-master/project/client/src/components/Footer.js
@@ -1,19 +1,26 @@
-import styled from "styled-components";
-import Register from "./Login/Register";
-
+import styled from 'styled-components';
+import Register from './Login/Register';
 
 const FooterArea = styled.footer`
   padding: 44px;
   background-color: ${({ theme }) => theme.colors.dark};
-`
+`;
 
 const StyledFooter = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr;
   margin: 0 auto 0 auto;
   @media (${({ theme }) => theme.scale.medium}) {
-  width: 1400px;
-}
+    width: 1400px;
+  }
+  @media (${({ theme }) => theme.scale.small}) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+  } ;
 `;
 
 const FooterBox1 = styled.section`
@@ -32,15 +39,15 @@ const FooterBox2 = styled.section`
 
 const Footer = () => (
   <FooterArea>
-  <StyledFooter>
-    <FooterBox1>
-      <p>THIS IS A FOOTER STILL IN UNDER CONSTRUCTION!</p>
-      <p>Empty Space: A New Hope for Space</p>
-    </FooterBox1>
-    <FooterBox2>
-      <Register />
-    </FooterBox2>
-  </StyledFooter>
+    <StyledFooter>
+      <FooterBox1>
+        <p>THIS IS A FOOTER STILL IN UNDER CONSTRUCTION!</p>
+        <p>Empty Space: A New Hope for Space</p>
+      </FooterBox1>
+      <FooterBox2>
+        <Register />
+      </FooterBox2>
+    </StyledFooter>
   </FooterArea>
 );
 
