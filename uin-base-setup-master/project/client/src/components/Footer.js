dc9698c36ea89a793a9a32c69c473c0ef74f4ab2
EricMichaelLP <Eric-m.l.p@hotmail.com>
2021-05-12 21:38:00 +0200

    FooterStyle

--- uin-base-setup-master/project/client/src/components/Footer.js
+++ uin-base-setup-master/project/client/src/components/Footer.js
@@ -4,6 +4,7 @@ import Register from './Login/Register';
 const FooterArea = styled.footer`
   padding: 44px;
   background-color: ${({ theme }) => theme.colors.dark};
+  margin-top: 255px;
 `;
 
 const StyledFooter = styled.section`
@@ -12,6 +13,12 @@ const StyledFooter = styled.section`
   margin: 0 auto 0 auto;
   @media (${({ theme }) => theme.scale.medium}) {
     width: 1400px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
   }
   @media (${({ theme }) => theme.scale.small}) {
     display: flex;
@@ -28,6 +35,17 @@ const FooterBox1 = styled.section`
   color: ${({ theme }) => theme.colors.light};
   display: block;
   grid-columns: 1;
+  margin-right: 10rem;
+
+  @media (${({ theme }) => theme.scale.small}) {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    margin-right: 5rem;
+  } ;
 `;
 
 const FooterBox2 = styled.section`
@@ -35,14 +53,30 @@ const FooterBox2 = styled.section`
   color: ${({ theme }) => theme.colors.light};
   display: block;
   grid-columns: 2;
+
+  @media (${({ theme }) => theme.scale.small}) {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+  } ;
+`;
+
+const KontaktFooter = styled.p`
+  margin-bottom: 2.5rem;
 `;
 
 const Footer = () => (
   <FooterArea>
     <StyledFooter>
       <FooterBox1>
-        <p>THIS IS A FOOTER STILL IN UNDER CONSTRUCTION!</p>
-        <p>Empty Space: A New Hope for Space</p>
+        <KontaktFooter>Outland AS</KontaktFooter>
+        <KontaktFooter>Kirkegata 23 0153 Oslo</KontaktFooter>
+        <KontaktFooter>kundeservice@outland.no</KontaktFooter>
+        <KontaktFooter>Kontakt - 55 31 48 22</KontaktFooter>
+        <KontaktFooter>Mandag–onsdag: 10.00–18.00</KontaktFooter>
       </FooterBox1>
       <FooterBox2>
         <Register />
